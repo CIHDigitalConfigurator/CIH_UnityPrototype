d74ad82355f58f34bf765e769601d9434e1d341d
buka71 <nataliaw@tutanota.com>
2021-02-15 16:24:15 +0000

    Vector2D boundary vertices output
    Signed-off-by: buka71 <nataliaw@tutanota.com>

---
+++ Assets/Scripts/UI/RaycastBlocker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RaycastBlocker : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+        //private bool raycastBlock = false;
+    private RaycastController raycastController;
+
+        void Start() 
+        {
+        raycastController = GameObject.FindGameObjectWithTag("eventSystem").GetComponent<RaycastController>();
+        }
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            raycastController.RayBlocked = true;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            raycastController.RayBlocked =  false;
+        }
+ }
