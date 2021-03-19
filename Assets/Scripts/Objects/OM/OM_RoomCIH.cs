305dfa4bc2d6032374c5619e5315167f267e3829
buka71 <nataliaw@tutanota.com>
2021-03-19 11:30:23 +0000

    Added BHoM CIH support

---
+++ Assets/Scripts/Objects/OM/OM_RoomCIH.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BH.oM.Base;
+using BH.oM.Geometry;
+
+public class RoomCIH : MonoBehaviour, IObject
+{
+    public string TypologyName { get; set; }
+    public float MinArea { get; set; }
+    public bool IsCirculation { get; set; }
+    public float DaylightFactor { get; set; }
+    public int AcousticSeparation { get; set; }
+    
+    /// <remarks>
+    /// The following properties are associated with the 
+    /// individual rooms generated in Module 2
+    /// Polyline can be replaced with list of tuples
+    /// </remarks>
+    public string RoomName { get; set; }
+    public Polyline Outline { get; set; }
+    public List<bool> EdgesExternal { get; set; }
+    public float Height { get; set; }
+    public int Level { get; set; }
+
+}
