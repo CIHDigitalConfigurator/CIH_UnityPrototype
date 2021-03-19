fcab73f940ff9849ad0c4cff27d41064034192a4
buka71 <nataliaw@tutanota.com>
2021-03-19 11:30:23 +0000

    Added BHoM CIH support

---
+++ Assets/Scripts/Solver/ApplySpec.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BH.oM.Base;
+using BH.oM.CIH;
+
+
+public class ApplySpec : MonoBehaviour, ISpecification
+{
+
+}
